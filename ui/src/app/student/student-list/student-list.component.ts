baec8858c0d9a0293fb8a6050b4fd7b7f0915a0a
balraj hanmanthugari <balraj.hanmanthugari@gmail.com>
2024-11-09 02:21:29 +0530

    ag grid table changes

--- ui/src/app/student/student-list/student-list.component.ts
+++ ui/src/app/student/student-list/student-list.component.ts
@@ -8,11 +8,19 @@ import { Component } from '@angular/core';
 export class StudentListComponent {
   
   columnDefs: any = [
-    { headerName: 'First Name', field: 'firstName', editable: true },
-    { headerName: 'Last Name', field: 'lastName' }
+    { headerName: 'First Name', field: 'firstName', flex: 1, editable: true, filter: true },
+    { headerName: 'Last Name', field: 'lastName', flex: 1 }
   ];
 
-  rowData = [
+  rowSelection: any = {
+    mode: 'multiRow',
+  };
+
+  pagination = true;
+  paginationPageSize = 200;
+  paginationPageSizeSelector = [200, 500, 1000];
+
+  rowData: any = [
     { firstName: 'Toyota', lastName: 'Celica' },
     { firstName: 'Ford', lastName: 'Mondeo' },
     { firstName: 'Porsche', lastName: 'Boxter' }
