e8201cdfc4202cb0441dcf3df373d3c8de4c370d
balraj hanmanthugari <balraj.hanmanthugari@gmail.com>
2024-11-09 01:59:08 +0530

    AG Grid in angular

--- ui/src/app/student/student-list/student-list.component.ts
+++ ui/src/app/student/student-list/student-list.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-student-list',
   templateUrl: './student-list.component.html',
-  styleUrl: './student-list.component.scss'
+  styleUrl: './student-list.component.scss',
 })
 export class StudentListComponent {
+  
+  columnDefs: any = [
+    { headerName: 'First Name', field: 'firstName', editable: true },
+    { headerName: 'Last Name', field: 'lastName' }
+  ];
 
+  rowData = [
+    { firstName: 'Toyota', lastName: 'Celica' },
+    { firstName: 'Ford', lastName: 'Mondeo' },
+    { firstName: 'Porsche', lastName: 'Boxter' }
+  ];
 }
--- ui/src/app/student/student.module.ts
+++ ui/src/app/student/student.module.ts
@@ -4,6 +4,7 @@ import { StudentFormComponent } from './student-form/student-form.component';
 import { StudentListComponent } from './student-list/student-list.component';
 import { StudentService } from './student.service';
 import { RouterModule, Routes } from '@angular/router';
+import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
 
 const routes: Routes = [
   {
@@ -26,6 +27,7 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    AgGridAngular,
     RouterModule.forChild(routes)
   ]
 })
