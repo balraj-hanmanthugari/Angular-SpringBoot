4353b27a6fa08fdae4841dcb95addd0cd60f41c0
balraj hanmanthugari <balraj.hanmanthugari@gmail.com>
2024-11-26 20:55:43 +0530

    vspring boot version change

--- src/main/java/com/fullstack/ems/controller/EmployeeController.java
+++ src/main/java/com/fullstack/ems/controller/EmployeeController.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,8 +16,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.fullstack.ems.entity.Employee;
 import com.fullstack.ems.service.EmployeeServiceImpl;
 
